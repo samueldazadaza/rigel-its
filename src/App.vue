7101006ab662fe982119b62a8df3bb5c6a2f0f77
samueldazadaza <65508457+samueldazadaza@users.noreply.github.com>
2022-10-07 09:05:34 -0500

    vista rigel ok todos los sistemas

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <input
-      class="m-2"
-      type="text"
-      placeholder="Search ID movil"
-      class="search-input"
-      v-model="searchValue"
-    />
-    {{ searchValue }}
+    <div class="table-responsive-sm container-lg" id="contenedor-tabla">
+      <input
+        class="m-2 input"
+        type="text"
+        placeholder="Search ID movil"
+        class="search-input"
+        v-model="searchValue"
+      />
+      {{ searchValue }}
 
-    <div class="table-responsive-sm">
-      <table class="table table-bordered table-hover table-sm">
+      <table class="table table-bordered table-hover table-sm fs-6">
         <thead class="">
           <tr>
             <th>#</th>
@@ -19,21 +19,31 @@
             <td>Description</td>
             <td>Date</td>
             <td>Days</td>
+            <td>Inmovilized</td>
           </tr>
         </thead>
 
-        <tbody class="">
+        <tbody class="fs-6">
           <tr
             v-for="(p, index) in post"
-            v-bind:style="p.system_name == 'IT-SIRCI' ? 'color:green' : ''"
+            v-bind:style="p.system_name == 'IT-SIRCI' ? 'color:blue' : ''"
             v-bind:style="p.system_name == 'IT-ITS' ? 'color:red' : ''"
           >
             <th>{{ index + 1 }}</th>
             <td>{{ p.vehicle_code }}</td>
-            <td v-text="p.system_name"></td>
+            <td>{{ p.system_name }}</td>
             <td>{{ p.issue_description }}</td>
             <td>{{ p.date_created }}</td>
             <td>{{ p.days_off }}</td>
+            <td
+              v-bind:style="
+                p.is_inmovilized_vehicle == true
+                  ? 'background-color: #FBB0A0'
+                  : ''
+              "
+            >
+              {{ p.is_inmovilized_vehicle }}
+            </td>
           </tr>
         </tbody>
       </table>
